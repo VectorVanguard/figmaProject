1aacc4bbb3bae9ea8c1aef8aaafae977a6ab82de
Egorick <gegor433@gmail.com>
2025-05-22 01:14:21 +0300

    Почти сделал

--- app/src/main/java/com/example/projectfigma/Activity/OrderConfirmed.kt
+++ app/src/main/java/com/example/projectfigma/Activity/OrderConfirmed.kt
@@ -1,8 +1,6 @@
 package com.example.projectfigma.Activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,27 +9,16 @@ import com.example.projectfigma.Fragments.BottomPanelFragment
 import com.example.projectfigma.R
 import com.example.projectfigma.Util.StatusBar
 
-class PaymentActivity : AppCompatActivity() {
+class OrderConfirmed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_payment)
+        setContentView(R.layout.activity_order_confirmed)
 
         StatusBar.hideStatusBar(window)
-
-        val image : ImageView = findViewById(R.id.payNow)
-        image.setOnClickListener(){
-            openActivity(OrderConfirmed::class.java)
-        }
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.buttonPanel, BottomPanelFragment())
             .commit()
-    }
-
-    private fun <T> openActivity(activityClass: Class<T>) {
-        val ctx = this
-        val intent = Intent(ctx, activityClass)
-        startActivity(intent)
     }
 }
